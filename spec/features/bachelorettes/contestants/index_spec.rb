c688b00683ac929cc4668f79e22caa9048884650
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-01 13:56:13 -0600

    Set up bachelorette show page and bachelorette's contestants index page specs

---
+++ spec/features/bachelorettes/contestants/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a bachelorette's contestants index page", type: :feature do
+  before :each do
+    @bachelorette_1 = Bachelorette.create!(name: "Hannah Brown", season_number: 15, season_description: "The Most Dramatic Season Yet!")
+    @bachelorette_2 = Bachelorette.create!(name: "Suzie Q", season_number: 11, season_description: "Less dramatic than season 15!")
+    @contestant_1 = @bachelorette_1.contestants.create!(name: "Pilot Pete", age: 34, hometown: "Irving, TX")
+    @contestant_2 = @bachelorette_1.contestants.create!(name: "Ben Higgins", age: 28, hometown: "Los Angeles, CA")
+    @contestant_3 = @bachelorette_2.contestants.create!(name: "Another Contestant", age: 24, hometown: "Denver, CO")
+  end
+
+  it "I only see that bachelortette's contestants." do
+    visit "/bachelorettes/#{@bachelorette_1}"
+    click_on "#{@bachelorette_1.name}'s Contestants"
+    expect(current_path).to eq("/bachelorettes/#{@bachelorette_1}/contestants")
+  end
+end
