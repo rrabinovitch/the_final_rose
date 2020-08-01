c6d24bc1af28a7f4185002a81d7a62c4f584d566
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-01 13:48:07 -0600

    Add season description attribute to bachelorettes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_01_192912) do
+ActiveRecord::Schema.define(version: 2020_08_01_194708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_08_01_192912) do
     t.integer "season_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "season_description"
   end
 
   create_table "contestant_outings", force: :cascade do |t|
